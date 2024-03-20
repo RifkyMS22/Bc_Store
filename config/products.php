dd79841c66675f186b76a17e6b918cea5412ce5b
RifkyMS22 <rifkymuhammadshodiq22@gmail.com>
2024-03-21 02:38:54 +0700

    update

--- config/products.php
+++ config/products.php
@@ -4,49 +4,49 @@ return [
     [
         'id' => 1,
         'name' => 'iPhone 14 pro',
-        'price' => 1000,
+        'price' => 28499000,
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
         'image' => 'https://cdn.eraspace.com/pub/media/wysiwyg/ibox/IP_14_Pro.webp?rand=1695287682'
     ],
     [
         'id' => 2,
         'name' => 'iPhone 14',
-        'price' => 5999000,
+        'price' => 11999000,
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
         'image' => 'https://cdn.eraspace.com/pub/media/wysiwyg/ibox/IP_14.webp?rand=1695287682'
     ],
     [
         'id' => 3,
         'name' => 'iPhone 13 pro',
-        'price' => 4999000,
+        'price' => 11499000,
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
         'image' => 'https://cdn.eraspace.com/pub/media/wysiwyg/ibox/IP_13_Pro.webp?rand=1695287682'
     ],
     [
         'id' => 4,
         'name' => 'iPhone 13',
-        'price' => 3999000,
+        'price' => 11999000,
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
         'image' => 'https://cdn.eraspace.com/pub/media/wysiwyg/ibox/IP_13.webp?rand=1695287682'
     ],
     [
         'id' => 5,
         'name' => 'iPhone 12 pro',
-        'price' => 2999000,
+        'price' => 12999000,
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
         'image' => 'https://cdn.eraspace.com/pub/media/wysiwyg/ibox/IP_12_Pro.webp?rand=1695287682'
     ],
     [
         'id' => 6,
         'name' => 'iPhone 12',
-        'price' => 1999000,
+        'price' => 11999000,
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
         'image' => 'https://cdn.eraspace.com/pub/media/wysiwyg/ibox/12_pro.png'
     ],
     [
         'id' => 7,
         'name' => 'iPhone 11',
-        'price' => 1999000,
+        'price' => 10999000,
         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
         'image' => 'https://cdn.eraspace.com/pub/media/wysiwyg/ibox/IP_11.webp?rand=1695287682'
     ]
